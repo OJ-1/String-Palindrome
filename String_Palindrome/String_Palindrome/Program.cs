24a18c3657b04688cf3953b6268c9619b8e76313
OJ <89594218+OJ-1@users.noreply.github.com>
2021-08-26 17:05:09 +0200

    Add files via upload

---
+++ String_Palindrome/String_Palindrome/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace String_Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input_string = "";
+            Console.WriteLine("Enter string: ");
+            input_string = Console.ReadLine();
+
+            Program obj = new Program();
+            Console.WriteLine("result is: " + obj.isPalindrome(input_string));
+        }
+
+        public bool isPalindrome(string input_string)
+        {
+            //convert string to array of characters
+            char[] inverse_char = input_string.ToCharArray();
+
+            //reverse the string
+            Array.Reverse(inverse_char);
+
+            //place into temp string - convert into a string
+            string temp_string = new string(inverse_char);
+
+            //check condition
+            bool palindrome_check = input_string.Equals(temp_string, StringComparison.OrdinalIgnoreCase);// returns true or false
+
+            return palindrome_check;
+        }
+
+    }
+}
